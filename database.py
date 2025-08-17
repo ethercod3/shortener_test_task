ac679fe5884b915d77f585bdf492ffe7a5acbce2
Etherial <typicaluserinfinite@gmail.com>
2025-08-17 15:07:35 +0900

    add db volume

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///database.db"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///./data/database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -9,10 +9,8 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-from contextlib import contextmanager
-
 def get_db():
-    db: Session = SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
